ec069176b38a7f4e78a3f7a90ff3ab8d66c12c90
unknown <ong.philippe@gmail.com>
2018-11-08 15:37:38 +0100

    blindage nom Philippe

--- handles.js
+++ handles.js
@@ -15,10 +15,10 @@ module.exports = {
       // explains how /hello works
       variable_content = "By setting '/hello' as path with a 'name' param, 'Hello [name]' will be printed. <br/>" +
       "Example : <a href='http://localhost:8080/hello?name=YourName'>http://localhost:8080/hello?name=YourName</a> will print 'Hello YourName' <br/><br/>" +
-      "By setting '/hello' as path with 'philippe' in the 'name' param, a short introduction of me will be printed. <br/>" +
+      "By setting '/hello' as path with 'philippe' or 'Philippe' in the 'name' param, a short introduction of me will be printed. <br/>" +
       "Example : <a href='http://localhost:8080/hello?name=philippe'>http://localhost:8080/hello?name=philippe</a> <br/><br/>" +
       "By setting in other path, a 404 code with a '404 Page not found' will be sent";
-    } else if (path === "/hello" && params["name"] === "philippe") {
+    } else if (path === "/hello" && (params["name"] === "philippe"||params["name"] === "Philippe")) {
       // short intro of yourself
       variable_content = "My name is Ong Philippe, I am 22 yo. <br/>" +
       "I am a engineering student in final year of master degree, specialized in IoT, networks and services";
