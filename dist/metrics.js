b7e1e5f1a8d090cd96ddcb7c831dd53a6f56d962
unknown <ong.philippe@gmail.com>
2018-11-24 22:33:56 +0100

    build pour mettre à jour le dist

--- dist/metrics.js
+++ dist/metrics.js
@@ -1,26 +1,67 @@
 "use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
 Object.defineProperty(exports, "__esModule", { value: true });
+const leveldb_1 = require("./leveldb");
+const level_ws_1 = __importDefault(require("level-ws"));
 class Metric {
     constructor(ts, v) {
-        this.timestamp = new Date(ts).getTime();
+        this.timestamp = ts;
         this.value = v;
     }
 }
 exports.Metric = Metric;
 class MetricsHandler {
-    static get(callback) {
-        callback(null, [
-            new Metric("2013-11-04 14:00 UTC", 12),
-            new Metric("2013-11-04 14:30 UTC", 15)
-        ]);
-    } // ? paramètre optionnel
+    constructor(path) {
+        this.db = leveldb_1.LevelDb.open(path);
+    }
+    save(key, met, callback) {
+        const stream = level_ws_1.default(this.db);
+        stream.on("error", callback);
+        stream.on("close", callback);
+        met.forEach(m => {
+            stream.write({ key: `metric:${key}:${m.timestamp}`, value: m.value });
+        });
+        stream.end();
+    }
+    delete(key, callback) {
+        const stream = this.db.createReadStream();
+        var met = [];
+        stream
+            .on("error", callback)
+            .on("end", (err) => {
+            callback(null, met);
+        })
+            .on("data", (data) => {
+            const [, k, timestamp] = data.key.split(":");
+            const value = data.value;
+            if (key != k) {
+                console.log(`Level DB error: ${data} does not match key ${key}`);
+            }
+            else {
+                this.db.del(data.key);
+            }
+        });
+    }
+    get(key, callback) {
+        const stream = this.db.createReadStream();
+        var met = [];
+        stream
+            .on("error", callback)
+            .on("end", (err) => {
+            callback(null, met);
+        })
+            .on("data", (data) => {
+            const [, k, timestamp] = data.key.split(":");
+            const value = data.value;
+            if (key != k) {
+                console.log(`Level DB error: ${data} does not match key ${key}`);
+            }
+            else {
+                met.push(new Metric(timestamp, value));
+            }
+        });
+    }
 }
 exports.MetricsHandler = MetricsHandler;
-module.exports = {
-    get: callback => {
-        callback(null, [
-            { timestamp: new Date("2013-11-04 14:00 UTC").getTime(), value: 12 },
-            { timestamp: new Date("2013-11-04 14:30 UTC").getTime(), value: 15 }
-        ]);
-    }
-};
--- dist/server.js
+++ dist/server.js
@@ -2,14 +2,18 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 const express = require("express");
 const metrics_1 = require("./metrics");
+const bodyparser = require("body-parser");
 const app = express();
 const port = process.env.PORT || "8080";
+const dbMet = new metrics_1.MetricsHandler("./db");
+app.use(bodyparser.json());
+app.use(bodyparser.urlencoded());
 app.get("/", (req, res) => {
     res.write("Hello world");
     res.send();
 });
-app.get("/metrics", (req, res) => {
-    metrics_1.MetricsHandler.get((err, result) => {
+app.get("/metrics/:id", (req, res) => {
+    dbMet.get(req.params.id, (err, result) => {
         if (err)
             throw err;
         if (result === undefined) {
@@ -18,6 +22,24 @@ app.get("/metrics", (req, res) => {
         }
         else
             res.json(result);
+    });
+});
+app.post("/metrics/:id", (req, res) => {
+    dbMet.save(req.params.id, req.body, (err) => {
+        if (err) {
+            res.status(500);
+            throw err;
+        }
+        res.status(200).send();
+    });
+});
+app.delete("/metrics/:id", (req, res) => {
+    dbMet.delete(req.params.id, (err) => {
+        if (err) {
+            res.status(500);
+            throw err;
+        }
+        res.status(200).send();
     });
 });
 app.listen(port, (err) => {
