2e1ba536f566173246068cf0f7933621d32ea684
Ong Philippe <ong.philippe@gmail.com>
2018-11-26 09:11:16 +0100

    Mis en place de tests unitaires : mocha, chai, travis

---
+++ src/metrics.test.ts
@@ -0,0 +1,11 @@
+import { expect } from "chai";
+
+const a: number = 0;
+
+describe("Metrics", function() {
+  describe("#get", function() {
+    it("should equal 0", function() {
+      expect(a).to.equal(1);
+    });
+  });
+});
