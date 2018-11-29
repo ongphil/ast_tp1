d5799f7e5d2bfa692c2dfc342c0b201cb306be4e
Ong Philippe <ong.philippe@gmail.com>
2018-11-29 19:37:51 +0100

    Ajout d'un user dans le populate pour faciliter les tests

--- bin/populate.ts
+++ bin/populate.ts
@@ -1,26 +1,25 @@
 #!/usr/bin/env ts-node
 
 import { MetricsHandler, Metric } from "../src/metrics";
+import { UserHandler, User } from "../src/users";
 
 const dbMet = new MetricsHandler("./db/metrics");
+const dbUser = new UserHandler("./db/users");
 
 const met = [
   new Metric(`${new Date("2013-11-04 14:00 UTC").getTime()}`, 12),
   new Metric(`${new Date("2013-11-04 14:15 UTC").getTime()}`, 10),
   new Metric(`${new Date("2013-11-04 14:30 UTC").getTime()}`, 8)
 ];
-const met1 = [
-  new Metric(`${new Date("2013-11-04 15:00 UTC").getTime()}`, 34),
-  new Metric(`${new Date("2013-11-04 15:15 UTC").getTime()}`, 23),
-  new Metric(`${new Date("2013-11-04 15:30 UTC").getTime()}`, 12)
-];
+
+const user = new User("user", "user@test.com", "user");
 
 dbMet.save("0", met, (err: Error | null) => {
   if (err) throw err;
-  console.log("Data populated");
+  console.log("Data metrics populated");
 });
 
-dbMet.save("1", met1, (err: Error | null) => {
+dbUser.save(user, (err: Error | null) => {
   if (err) throw err;
-  console.log("Data populated");
+  console.log("Data user populated");
 });
